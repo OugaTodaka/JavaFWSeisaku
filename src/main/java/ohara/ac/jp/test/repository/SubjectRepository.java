124f84f88bbde183baeb8d9e91aea2ffaea54938
JunyaOkada <fko2347022@stu.o-hara.ac.jp>
2024-04-18 12:03:55 +0900

    okadakousin5

--- src/main/java/ohara/ac/jp/test/repository/SubjectRepository.java
+++ src/main/java/ohara/ac/jp/test/repository/SubjectRepository.java
@@ -1,10 +1,10 @@
 package ohara.ac.jp.test.repository;
 
-import javax.security.auth.Subject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import ohara.ac.jp.test.model.Subject;
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject,Long>{
 
